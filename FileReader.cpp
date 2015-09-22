cc4e5f01fd16b08a24eb7183e4eec603af917ada
muzuka <seanbrown7935@gmail.com>
2015-09-22 15:39:30 -0600

    Finished FileReader

--- FileReader.cpp
+++ FileReader.cpp
@@ -12,6 +12,24 @@ FileReader::FileReader(string file) {
 
 BSpline FileReader::readBSpline() {
   if(!input.open()) {
-    
+    printf("Fatal file error\n");
+    exit(EXIT_FAILURE);
   }
+  
+  BSpline newSpline(3);
+  int numOfControlPoints;
+  double x, y, z, w;
+  
+  input >> numOfControlPoints;
+  
+  for(int i = 0; i < numOfControlPoints; i++) {
+    input >> x;
+    input >> y;
+    input >> z;
+    input >> w;
+    
+    newSpline.addPoint(controlPoint(x, y, z, w, 1.0f));
+  }
+  
+  return newSpline;
 }
