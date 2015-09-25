a6e6158ccabee49e76e1d609b972bdd3493a6456
muzuka <seanbrown7935@gmail.com>
2015-09-25 17:16:59 -0600

    Script that extracts control points

---
+++ CurveOutput.py
@@ -0,0 +1,34 @@
+
+# script to output curve control points
+# Steps
+#   -> get Scene
+#   -> get Curve
+#   -> get Control Points
+#   -> open file
+#   -> Write control points to file
+#
+#
+
+import bpy
+
+count = 0
+
+obj = bpy.data.objects["NurbsCurve"]
+
+curve = obj.data
+
+file = open('UltimateCurve.txt', 'w')
+
+for spline in curve.splines:
+    print(type(spline))
+    for point in spline.points:
+        count += 1
+        p = str.replace(str(point.co), '<Vector (', '')
+        p = str.replace(p, ')>', '')
+        p = str.replace(p, ',', '')
+        file.write(p + '\n')
+        print(p)
+
+print(count)
+
+file.close()
