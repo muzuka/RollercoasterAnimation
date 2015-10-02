e716495c926276a45adad1cf2314763da04abb31
muzuka <seanbrown7935@gmail.com>
2015-10-02 15:52:46 -0600

    created Rollercoaster.h

---
+++ Rollercoaster.h
@@ -0,0 +1,25 @@
+#ifndef ROLLERCOASTER_H
+#define ROLLERCOASTER_H
+
+/*
+ * CPSC 589
+ *
+ * Rollercoaster class
+ * 
+ * From CPSC 589
+ *
+ * Sean Brown
+ *
+ */
+ 
+#include "BSPline.h"
+
+class Rollercoaster : public BSpline {
+private:
+  std::vector<Trackpoint> tracks;
+  
+public:
+  
+}
+
+#endif
