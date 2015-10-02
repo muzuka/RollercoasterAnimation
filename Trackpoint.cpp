e963487fee7981046f09e9032c62687c9968a4a8
muzuka <seanbrown7935@gmail.com>
2015-10-02 15:53:10 -0600

    created Trackpoint.cpp

---
+++ Trackpoint.cpp
@@ -0,0 +1,37 @@
+
+/*
+ * CPSC 589
+ *
+ * Track Point class
+ * 
+ * From CPSC 589
+ *
+ * Sean Brown
+ *
+ */
+
+ Trackpoint::Trackpoint(Tracktype t, double x, double y, double z, double w, double s) {
+ 	this->type 	= t;
+ 	this->x 	= x;
+ 	this->y 	= y;
+ 	this->z 	= z;
+ 	this->weight = w;
+    this->size 	= s;
+ }
+
+Trackpoint::Trackpoint(Tracktype t, double x, double y, double z, double s) {
+ 	this->type 	= t;
+ 	this->x 	= x;
+ 	this->y 	= y;
+ 	this->z 	= z;
+    this->weight = 0.0f;
+    this->size 	= s;
+ }
+ 
+ Tracktype Trackpoint::getType() {
+	return type;
+}
+
+void Trackpoint::setType(Tracktype t) {
+	this->type = t;
+}
