56fe2496b07ffcb4d4127a94837b12220a9492b9
muzuka <seanbrown7935@gmail.com>
2015-10-08 16:51:31 -0600

    created frenet class

---
+++ Frenetframe.cpp
@@ -0,0 +1,48 @@
+
+#include "Frenetframe.h"
+
+Frenetframe::Frenetframe() {
+  this->t = Vertex();
+  this->n = Vertex();
+  this->b = Vertex();
+}
+
+Frenetframe::Frenetframe(Vertex t, Vertex n, Vertex, b) {
+  this->t = t;
+  this->n = n;
+  this->b = b;
+}
+
+Vertex Frenetframe::getTangent() {
+  return t;
+}
+
+Vertex Frenetframe::getNormal() {
+  return n;
+}
+
+Vertex Frenetframe::getBinormal() {
+  return b;
+}
+
+void Frenetframe::setTangent(Vertex t) {
+  this->t = t;
+}
+
+void Frenetframe::setNormal(Vertex n) {
+  this->n = n;
+}
+
+void Frenetframe::setBinormal(Vertex b) {
+  this->b = b;
+}
+
+Fernetframe Frenetframe::computeFrenet(Vertex first, Vertex second, Vertex, last) {
+  double h = ((last - (second * 2) + first) / 2).length();
+  double c = (last - first).length();
+  double r = (c * c) + (4 * (h * h)) / (8 * h);
+  
+  this->t = last - first;
+  
+  this->n = 
+}
---
+++ Frenetframe.h
@@ -0,0 +1,29 @@
+#ifndef FRENETFRAME_H
+#define FRENETFRAME_H
+
+#include "Vertex.h"
+
+class Frenetframe {
+private:
+
+  Vertex t; // tangent
+  Vertex n; // normal
+  Vertex b; // binormal
+
+public:
+
+  Frenetframe();
+  Frenetframe(Vertex, Vertex, Vertex);
+  
+  Vertex getTangent();
+  Vertex getNormal();
+  Vertex getBinormal();
+  
+  void setTangent(Vertex);
+  void setNormal(Vertex);
+  void setBinormal(Vertex);
+  
+  static Frenetframe computeFrenet(Vertex, Vertex, Vertex);
+};
+
+#endif
